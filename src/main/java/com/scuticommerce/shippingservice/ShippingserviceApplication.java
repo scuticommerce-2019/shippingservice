f6a3c6cbbd3d80be7cdd1ff76e00fb7c38605d8d
Kapil Kataria <kapil.kataria@kp.org>
2019-03-15 20:19:36 -0700

    first commit

---
+++ src/main/java/com/scuticommerce/shippingservice/ShippingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.scuticommerce.shippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scuticommerce/shippingservice/ShippingserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scuticommerce.shippingservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShippingserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
